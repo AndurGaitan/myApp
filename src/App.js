8fe1937752e3f79e4c45f1336db441ac6e448666
AndurGaitan <andresdavidagaitan@hotmail.com>
2022-08-23 06:43:19 -0300

    agregando compontentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './componentes/Footer';
+import Header from './componentes/header';
 
 function App() {
+  let imagen = "kqXt7Sq2.png";
+  let titulo = "Hamburguesa con Queso";
+  let calorias = "289 kcal";
+  let description = "Carne 100% vacuna, queso derretido y mostaza, kétchup y cebolla triturada, es algo que nunca puede fallar. Un clásico que nunca pasa de moda";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div> <Header/></div>
+      <div className="row"></div>
+      <div className='col-md-4 offset-md-2'>
+        <img src={imagen} alt={titulo} className="img-fluid" />
+      </div>
+      <div className='col-md-4'>
+        <h1>{titulo}</h1>
+      <p>{calorias}</p>
+      <p>{description}</p>
+      </div>
+    <Footer/>
     </div>
   );
 }
